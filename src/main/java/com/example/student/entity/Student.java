4c3c96b7a0264e49d1860333f6929cbc9bf7c83c
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-02 22:54:40 +0530

    Applied one to one mapping with the student

--- src/main/java/com/example/student/entity/Student.java
+++ src/main/java/com/example/student/entity/Student.java
@@ -1,8 +1,6 @@
 package com.example.student.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,4 +19,8 @@ public class Student {
 
     private String name;
     private int age;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
 }
